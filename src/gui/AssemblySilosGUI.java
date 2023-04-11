31800249b47936ede53ce5bbfa0e712ef86d04c1
Luke <lmcdougall@unm.edu>
2023-04-11 16:06:03 -0600

    Added label display of streams

--- src/gui/AssemblySilosGUI.java
+++ src/gui/AssemblySilosGUI.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
